5a297c15e40f33b6b6e9ed38edeca331e541f8a9
AlanGarci4 <alangarciasantos@outlook.com>
2024-07-03 15:20:29 -0300

    Inclusão de tratamento de exceção na entrada de dados, resitração única para entrada de dados. E otimização do README.md.

--- Questao_7.js
+++ Questao_7.js
@@ -1,14 +1,21 @@
+//Vincular código JavaScript a um botão no html, antetando-se para o comando "document.querySelector('ID DO BOTÃO')",
+//incluindo add.EventListner('click', function(){}); Considerar a criação da variável(btn1) e sua classe (btn1).
 const btn1 = document.querySelector('#Exercicio7');
 
 btn1.addEventListener('click', function(){
   //Lendo as notas. Observação: fora informado como const apenas para aumentar a prática da utilização do tipo
 //de variável de entrada a ser informado. Comando "parseFloat" encadeado com o comando "prompt", faz com que na tela
 //seja possível receber o dado e exibir a mensagem para o uusário.
-const nota1 = parseFloat(prompt("Digite a primeira nota: "));
-const nota2 = parseFloat(prompt("Digite a segunda nota: "));
-const nota3 = parseFloat(prompt("Digite a terceira nota: "));
+  nota1 = parseFloat(prompt("Digite a primeira nota: "));
+  nota2 = parseFloat(prompt("Digite a segunda nota: "));
+  nota3 = parseFloat(prompt("Digite a terceira nota: "));
 
-
+  //Tratar valores que sejam inclusos diferentes de 0.00 a 10.00
+  while (isNaN(nota1) || isNaN(nota2) || isNaN(nota3) || nota1 < 0 || nota1 > 10 || nota2 < 0 || nota2 > 10 || nota3 < 0 || nota3 > 10) {
+    alert("Digite Valores Válidos para as Notas, Contidos Entre Zero a Dez! Sendo NÃO Nulos ou em Branco!");
+    return;    
+  }
+   
 // A nota a ser utilizada serão apenas as duas miores, então precisamos descartar uma nota, para isso as estruturas
 // de decisão podem nos ajudar, em questão foi implementado com if e else, se e senão (senão se), a fim de praticar 
 //essa estrutura de condição, é possível ser feito por outras estruturas a depender da lógica que for aplicada pelo
@@ -35,7 +42,7 @@ if (nota1 >= nota2 && nota1 >= nota3) {
       maiorNota2 = nota2;
     }
   }
-  
+ 
   // Calcule a média das duas maiores notas
    media = (maiorNota1 + maiorNota2) / 2;
   
@@ -47,7 +54,10 @@ if (nota1 >= nota2 && nota1 >= nota3) {
   } else {
     alert(`Reprovado! Média ` + media);
   }
+
 });
+
+
 
 
 
--- Questao_8.js
+++ Questao_8.js
@@ -1,13 +1,23 @@
+//Vincular código JavaScript a um botão no html, antetando-se para o comando "document.querySelector('ID DO BOTÃO')",
+//incluindo add.EventListner('click', function(){}); Considerar a criação da variável(btn2) e sua classe (btn2).
 const btn2 = document.querySelector('#Exercicio8');
 
 btn2.addEventListener('click', function(){
 
+//Leitura do valor de salário a ser recebido para processamento.E declaração de variáveis constantes para o percental
+//por faixas de valores para aumento salarial.    
 const salarios = parseFloat(prompt("Digite o Salário Atual: " ));
 const percentual1 = 20;
 const percentual2 = 15;
 const percentual3 = 10;
 const percentual4 = 5;
 
+//Tratamento de valores que não são condizentes com a entrada de dados no valor do salário, Variavel salarios.
+while (isNaN(salarios) || salarios <= 0) {
+      alert("Digite um valor numérico válido para o salário!");
+      return;
+    }  
+//Aplicação do processamento dos aumentos salariais conforme condições preestabelecidas para o processamento.
 if (salarios <= 280.00){
     novoSalario = ((salarios * percentual1)/100) + salarios;
     alert("O Valor do Percentual Aplicado foi de: " + (percentual1 + '%'));
